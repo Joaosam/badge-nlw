ae4b97e07b78034d9f5ffcebd3af03fca5cb3812
João Pedro Mendes dos Santos <66846607+Joaosam@users.noreply.github.com>
2022-04-18 19:25:54 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const userSocialMedia = {
   github: 'Joaosam',
-  youtube: 'joao99jp',
-  instagram: 'jpms_jp',
-  facebook: 'joaopedro.mendes.161',
-  twitter: 'joaosan_jp'
+  linkedin:
+    'https://www.linkedin.com/in/joão-pedro-mendes-dos-santos-3b7b3a121',
+  instagram: 'https://www.instagram.com/jpms_jp',
+  facebook: 'https://www.facebook.com/joaopedro.mendes.161',
+  twitter: 'https://twitter.com/joaosan_jp'
 }
 
 function changeSocialMediaLinks() {
@@ -12,7 +13,7 @@ function changeSocialMediaLinks() {
     // Acesso as classes de cada filha
     const social = li.getAttribute('class')
     // Acesso o primeiro elemento da respectiva classe e atribuo a rede social e usuário aos links. O "userSocialMedia[social]" faz o seguinte. Olho o objeto userSocialMedia e verifico qual o usuário da rede social em questão.
-    li.children[0].href = `https://${social}.com/${userSocialMedia[social]}`
+    li.children[0].href = `${userSocialMedia[social]}`
   }
 }
 changeSocialMediaLinks()
